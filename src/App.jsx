32e28e51417c51355d0b3366e7a8915625ce5165
Kaiser <ricoreven@gmail.com>
2024-10-25 19:49:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { GiAbstract007 } from "react-icons/gi";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <button className='btn'>asd <GiAbstract007></GiAbstract007> </button>
+    </>
+  );
+}
+
+export default App;
